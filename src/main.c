f2c4492dfffb5be5b84e3b9f3d587ab1e50f503e
xperrin <xperrin@student.42.fr>
2018-07-16 14:20:50 +0200

    Linked list file parsing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/07 17:49:38 by xperrin           #+#    #+#             */
-/*   Updated: 2018/07/10 22:11:27 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/07/21 15:10:40 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,30 @@ static	size_t	parse_ant_number(void)
 	return (0);
 }
 
+static int		read_input(t_list **list)
+{
+	char	*line;
+	t_list	*next_list;
+	t_list	*tmp_list;
+
+	if (get_next_line(0, &line) <= 0)
+	{
+		free(line);
+		return (0);
+	}
+	*list = ft_lstnew(line, sizeof(char*));
+	tmp_list = *list;
+	free(line);
+	while (get_next_line(0, &line) > 0)
+	{
+		next_list = ft_lstnew(line, sizeof(char*));
+		tmp_list->next = next_list;
+		tmp_list = tmp_list->next;
+		free(line);
+	}
+	return (1);
+}
+
 int				print_error(void)
 {
 	ft_putstr(ERROR_S);
@@ -38,10 +62,16 @@ int				main(void)
 {
 	t_info	i;
 	t_list	rooms;
+	t_list	*input;
 
 	if (!(i.ants = parse_ant_number()))
 		return (print_error());
 	ft_printf("%d\n", i.ants);
-	parse_rooms(i, rooms);
+	read_input(&input);
+	while (input)
+	{
+		ft_printf("content: %s next:%d\n", input->content, input->next);
+		input = input->next;
+	}
 	return (0);
 }
