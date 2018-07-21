b16f5a582fb2a4e4cf36039987324e377aafdcb8
xperrin <xperrin@student.42.fr>
2018-07-21 14:20:50 +0200

    is_room predicate

--- src/parse_rooms.c
+++ src/parse_rooms.c
@@ -6,7 +6,7 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/10 18:16:40 by xperrin           #+#    #+#             */
-/*   Updated: 2018/07/22 21:57:10 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/07/22 22:51:19 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 static	int	is_room(t_list *input)
 {
+	size_t	i;
+	char	*s;
+
+	i = 0;
+	s = (char*)input->content;
+	while (s[i] && ft_isalnum(s[i]))
+		i++;
+	if (!i || s[i] != ' ')
+		return (0);
+	while (ft_isalnum(s[i]))
+		i++;
+	if (!s[i] || s[i] != ' ' || !s[i + 1] || !ft_isalnum(s[i + 1]))
+		return (0);
 	return (1);
 }
 
@@ -36,5 +49,6 @@ int			parse_rooms(t_list *input, t_info *info, t_list **rooms)
 			ft_putendl(input->content);
 		input = input->next;
 	}
+	ft_printf("%d\n", room_count);
 	return (1);
 }
