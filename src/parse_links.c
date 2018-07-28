bf18bb83fc662c9a10ee7b8db1dc8d1f068a2279
xperrin <xperrin@student.42.fr>
2018-07-28 18:56:26 +0200

    is_link predicate

--- src/parse_links.c
+++ src/parse_links.c
@@ -6,14 +6,35 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/26 16:13:55 by xperrin           #+#    #+#             */
-/*   Updated: 2018/07/26 16:22:41 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/07/28 18:55:23 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
+static	int		is_link(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && ft_isalnum(s[i]))
+		i++;
+	if (!i || !s[i] || s[i] != '-'
+		|| !s[i + 1] || !ft_isalnum(s[i + 1]))
+		return (0);
+	return (1);
+
+}
+
 int		parse_links(t_list *input, t_list **rooms)
 {
-	(void)input; (void)rooms;
+	while (input)
+	{
+		if (is_link(input->content))
+		{
+			ft_putendl(input->content);
+		}
+		input = input->next;
+	}
 	return (1);
 }
